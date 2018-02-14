2a112840dc1821c6aca9c8df3be8c606e1b9634a
Sara <sara.m.mccarthy@gmail.com>
2018-02-13 22:36:07 -0800

    u

--- assets/js/myClick.js
+++ assets/js/myClick.js
@@ -1,6 +1,6 @@
 function myClick() {
     var x = document.getElementsByClassName("post");
-    for(var i =0, il = selects.length;i<il;i++){
+    for(var i =0, il = x.length;i<il;i++){
     	x[i].style.color = "blue";
 	    if (x[i].style.display === "none") {
 	        x[i].style.display = "flex";
