bd46d633264c9175641c5516dff04abbe0d1ac77
Sara <sara.m.mccarthy@gmail.com>
2018-02-13 16:35:20 -0800

    u

--- assets/js/myClick.js
+++ assets/js/myClick.js
@@ -1,18 +1,13 @@
 function myClick(c) {
     var x = document.getElementsByClassName(c);
     for(var i =0, il = selects.length;i<il;i++){
-	movement.animate(
-	    20,
-	    function(){
-	    	x[i].style.background-color: rgba(255,255,255,0.1);
 
-	      	if (x[i].style.display === "none") {
-		        x[i].style.display = "flex";
-		    } else {
-		        x[i].style.display = "none";
-		    }
-	    });
-
+	    if (x[i].style.display === "none") {
+	        x[i].style.display = "flex";
+	    } else {
+	        x[i].style.display = "none";
+	    }
 	}
 }
+
 
