7586d92a927b579a0c6234cb686fa079ae47ad63
Sara <sara.m.mccarthy@gmail.com>
2018-02-13 23:06:25 -0800

    u

--- assets/js/myClick.js
+++ assets/js/myClick.js
@@ -1,6 +1,9 @@
-function myClick(type) {
+function myClick(i) {
     var x = document.getElementsByClassName("post");
-
+    var type;
+	if (i == 1){
+		type = "sticky"
+	}
     for(var i =0, il = x.length;i<il;i++){
 	    if (x[i].classList.contains(type)) {
 	        x[i].style.display = "flex";
@@ -9,5 +12,3 @@ function myClick(type) {
 	    }
 	}
 }
-
-
